b319c9791fcb484a005ac53563d2355d33f221eb
Tom11B <thomas.peterson@outlook.com>
2015-07-31 01:06:03 -0400

    Update sda-0.4.sh

--- sda-0.4.sh
+++ sda-0.4.sh
@@ -1,136 +1,119 @@
 #!/bin/bash
-# USB-Sheepdip:  Prototype sheep dip appliance for USB security in airgapped environments to be run on a Raspberry FGS
+#
+# USB-Sheepdip:  Prototype sheep dip appliance for USB security in airgapped environments to be run on a Raspberry fgstest
 # Authors:  Thomas Peterson <tpeterson@foxguardsolutions.com> and Charisse Rigdon (asv0r) <crigdon@foxguardsolutions.com>
 # Version: 0.4 July 2015
 #
 
 
 initializeLog () {		#  append header for this run to log
-
-	printf "\n\n===================================[ SDA Start : %s]============================================\n\n" , "$(date + %c)"| tee -a log.txt
+	printf "\n\n===================================[ sda Start : `date +%c` ]============================================\n\n" | tee -a log.txt
 	
 }
 
 checkMountPoints () {
         
-     sudo mount -a
-     df -Th | grep sd	
-     printf "\nAre these mount points correct? (y/n): "	
-     read -r input
-		
-        	if  [ $input != y ] or [ $input != Y ]
-			then
-				printf "Exiting due to incorrect mount points.\n"  | tee -a ./log.txt
-				exit 1
-        	fi
+        sudo mount -a
+        df -Th | grep sd
+        printf "\nAre these mount points correct? (y/n): "
+        read input
+        if  [ "$input" != "y" ]
+                then
+                        printf "Exiting due to incorrect mount points.\n"  | tee -a ./log.txt
+                        exit 1
+        fi
 }
 
 
 updateAV () {			#  update clamav definitions -- * not implemented yet *
 
-	printf  "Updating anti-virus definitions... \n" | tee -a ./log.txt
+	printf "`date +%T`:  Updating anti-virus definitions... \n" | tee -a ./log.txt
 	sudo freshclam
-		if [ $? == 0 ] 
-			then 
-				printf "%s: Update successful.\n" , "$(date +%T)" | tee -a ./log.txt 
-		else 
-				printf "%s: Update failed, please check the location of the update file on the outside USB drive.\n" , "$(date +%T)" | tee -a ./log.txt exit 1
-				exit 1 
-		fi
-}				
+	if [ $? eq 0 ] 
+	        then
+	         printf "Update successful.\n" | tee -a log.txt
+	fi
+}
 
 scanOutside () {		#  scan the outside USB drive, display infected files, remove them and write the output to a log file in the users home directory home/sda
 
-	printf "%s:  Scanning the outside USB now... \n", "$(date +%T)" | tee -a ./log.txt
-	sudo clamscan -v -r /home/FGS/USBs/outside | tee -a ./log.txt
-		if [ $? == 0 ]
-			then 
-				printf "** Virus scan complete no threat found **\n" | tee -a ./log.txt 
-			exit 1
-		elif [ $? == 1 ] 
-			then 	
-				printf "** VIRUSES FOUND.  DO NOT USE THESE FILES! **\n" | tee -a ./log.txt
-			exit 1
-		else
-			printf "Error occurred with virus scan.\n" | tee -a ./log.txt
-			exit 1
-		fi
+	printf "`date +%T`:  Scanning the outside USB now... \n" | tee -a ./log.txt
+	sudo clamscan -v -r /home/fgstest/USBs/outside | tee -a ./log.txt
+	
+	case $? in             # check exit code
+	       1) printf "** VIRUSES FOUND.  DO NOT USE THESE FILES! **\n" | tee -a ./log.txt; exit 1 ;;
+	       2) printf "Error occured with virus scan.\n" | tee -a ./log.txt; exit 1 ;;
+        esac
 }
-
 
 scanInside () {			#  scan the inside USB drive, display infected files, remove them and write the output to a log file in the users home directory home/sda
 
-	printf "%s:  Scanning the inside USB now... \n", "$(date +%T)" | tee -a ./log.txt
-	sudo clamscan -v -r /home/FGS/USBs/inside | tee -a ./log.txt
-		if [ $? == 0 ]
-			then 
-				printf "** Virus scan complete no threat found **\n" | tee -a ./log.txt 
-				exit 1
-		elif [ $? == 1 ] 
-			then 
-				printf "** VIRUSES FOUND.  DO NOT USE THESE FILES! **\n" | tee -a ./log.txt
-				exit 1
-		else
-				printf "Error occurred with virus scan.\n" | tee -a ./log.txt	
-				exit 1
-		fi
-}
+	printf "`date +%T`:  Scanning the inside USB now... \n" | tee -a ./log.txt
+	sudo clamscan -v -r /home/fgstest/USBs/inside | tee -a ./log.txt
 	
+	case $? in             # check exit code
+	       1) printf "** VIRUSES FOUND.  DO NOT USE THESE FILES! **\n" | tee -a ./log.txt ; exit 1 ;;
+	       2) printf "Error occured with virus scan.\n" | tee -a ./log.txt ; exit 1 ;;
+        esac
+}
 
 shredInside () {		#  secure erase the inside USB drive, the default setting writes 38 times and write the output to the log file
 
-	printf "%s : Shredding inside USB now... \n" , "$(date +%T)" | tee -a ./log.
-	sudo umount -v /dev/sdb1 | tee -a ./log.txt
-	sudo shred -v -z -n 0 /dev/sdb1 | tee -a ./log.txt		#  no overwriting; just one pass of writing zeroes
-		if [ $? != 0 ]
-			then 
-				printf "%s : ERROR: shred failed.  Please check logs before continuing. Attempting to format USB now... \n", "$(date +%T)"
-				exit 1
-		else
-				printf "Shred Complete... \n"
-		 fi
+	printf "`date +%T`:  Shredding inside USB now... \n" | tee -a ./log.txt
+	sudo umount -v /dev/sdb | tee -a ./log.txt
+	sudo shred -v -z -n 0 /dev/sdb | tee -a ./log.txt		#  no overwriting; just one pass of writing zeroes
+	
+	if [ $? != 0 ]
+                then
+                        printf "`date +%T`:  ERROR: shred failed.  Please check logs before continuing. Attempting to format USB now... \n"
+                        formatInside
+                        exit 1
+        fi
 	        
 }
 
 formatInside () {		#  format USB drive to FAT32 
 
-	printf "\n %s : Creating new FAT32 filesystem on inside USB...\n", "$(date +%T)"
-	sudo mkdosfs -v -F 32 -I /dev/sdb1 | tee -a ./log.txt 
-		if [ $? == 0 ]
-			then
-				printf "%s : USB formatted.\n" , "$(date +%T)"
-			else
-				printf "%s : ERROR: Unable to format drive.\n", "$(date +%T)"
-				exit 1
-		fi
+	printf "\n`date +%T`:  Creating new FAT32 filesystem on inside USB...\n"
+	sudo mkdosfs -v -F 32 -I /dev/sdb | tee -a ./log.txt
+	
+	if [ $? == 0 ]
+	        then
+	                printf "`date +%T`:  USB formatted.\n"
+                else
+                        printf "`date +%T`:  ERROR: Unable to format drive.\n"
+                        exit 1
+        fi
 }
 
 remount () {			#  remount the drive to prepare for copy
 
-	sudo mount -v -t vfat /dev/sdb1 /home/FGS/USBs/inside | tee -a ./log.txt
+	sudo mount -v -t vfat /dev/sdb /home/fgstest/USBs/inside | tee -a ./log.txt
 
 }
 
 copyOutsideToInside () {	# copy everything on outside USB to newly formatted inside USB
 
-	printf " %s : Copying USB now... \n" , "$(date +%T)"| tee -a ./log.txt 
-	sudo cp -v /home/FGS/USBs/outside/* /home/FGS/USBs/inside/ | tee -a ./log.txt
-	printf " %s:  Process complete. * WARNING: CHECK THE LOG FILE BEFORE CONTINUING *\n" , "$(date +%T)"
-	
+	printf "`date +%T`:  Copying USB now... \n" | tee -a ./log.txt
+	sudo cp -v /home/fgstest/USBs/outside/* /home/fgstest/USBs/inside/ | tee -a ./log.txt
+	printf "`date +%T`:  Process complete. * WARNING: CHECK THE LOG FILE BEFORE CONTINUING *\n"
 }
 
 unmount ()  {		# unmount USB drives so they can be safley removed 
 
-	printf " %s:Unmounting USB now standby... \n" ,"$(date +%T)" |tee -a ./log.txt 
-	sudo umount -v /dev/sda1 | tee -a ./log.txt
-	sudo umount -v /dev/sdb1 | tee -a ./log.txt
-	printf " %s:Unmount complete USBs can be safely removed * WARNING CHECK THE LOG FILE BEFORE CONTINUING *\n" , "$(date +%T)"
+	printf " `date +%T`:Unmounting USB now standby... \n" |tee -a ./log.txt 
+	
+	sudo umount -v /dev/sda | tee -a ./log.txt
+	
+	sudo umount -v /dev/sdb | tee -a ./log.txt
+	
+	printf "":Unmount complete USBs can be safely removed * WARNING CHECK THE LOG FILE BEFORE CONTINUING *\n" 
 	
 }
-	
-finishLog () {		#  append footer for this run to log
 
-printf "===================================[ End : %s ]============================================\n\n" | tee -a log.txt , "$(date +%T)"
+
+finishLog () {		#  append footer for this run to log
+	printf "===================================[ End : `date +%c` ]============================================\n\n" | tee -a log.txt
 	
 }
 
@@ -144,9 +127,14 @@ scanInside
 shredInside
 formatInside
 remount
-unmount
 copyOutsideToInside
+umount 
+
 finishLog
+
+
+
+
 
 
 
