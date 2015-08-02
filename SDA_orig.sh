341c08243677d0462734d1d563f3ca10502bbffc
Tom11B <thomas.peterson@outlook.com>
2015-08-02 12:00:13 -0400

    Update SDA_orig.sh
    This is the initial draft of the SDA script.

--- SDA_orig.sh
+++ SDA_orig.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 #SDA process,Thomas Peterson June 2015
 #Clamav, anti-virus software used to scan both the inside and outside USB drives
-#Secure-delete, utility used to erase the inside USB drive before copy
+#shred, utility used to erase the inside USB drive before copy
 #update Clamav before running a scan
 
 
 echo "Updating anti-virus definitions"
 
-#sudo freshclam
+sudo freshclam
 
 #scan the inside and outside USB drives, display infected files, remove them and write the output to a log file in the users home directory home/sda
 
 echo " Scanning the outside USB now"
 
-sudo clamscan -r /home/sda/outside_USB > /home/sda/log.txt
+  sudo clamscan -r /home/sda/outside_USB > /home/sda/log.txt
 
 echo "Scanning the inside USB now"
 
-sudo clamscan -r /home/sda/inside_USB >> /home/sda/log.txt
+  sudo clamscan -r /home/sda/inside_USB >> /home/sda/log.txt
 
 #secure erase the inside USB drive, the default setting writes 38 times and write the output to the log file
 
 echo "Shredding USB now"
 
-sudo shred -v -z -n 1 /dev/sdb1 >> /home/sda/log.txt
+  sudo shred -v -z -n 1 /dev/sdb1 >> /home/sda/log.txt
 
 echo "Copying USB now"
 
-cp -a /home/sda/outside_USB/. /home/sda/inside_USB/.
+  cp -a /home/sda/outside_USB/. /home/sda/inside_USB/.
 
 echo "Process complete * WARNING CHECK THE LOG FILE BEFORE CONTINUING *"
 
